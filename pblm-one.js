6e64bbe928e993099f880ccdd2d81b1818ffc853
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-26 14:10:42 +0600

    multiply number using arrow function

---
+++ pblm-one.js
@@ -0,0 +1,8 @@
+/**
+ * Write an arrow function that will 3 parameters, will      multiply the parameters and will return result.
+ */
+
+const multiply =(a, b, c) => a * b * c;
+
+const x = multiply(2,3,4);
+console.log(x)
