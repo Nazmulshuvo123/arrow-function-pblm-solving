5d9679cfd86b017c46f6cba402af12204ad4ab81
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-28 21:27:31 +0600

    suqare the array number , then sum of the number and find the average

--- problem-five.js
+++ problem-five.js
@@ -1,21 +1,17 @@
 /**
- * Write an arrow function where it will do the following: x suqre
+ * Write an arrow function where it will do the following: x ^2
  * 1. Square each array elements
  * 2. Calculate the sum of the square elements
  * 3. Return the average of the sum of the squared elements
  */
 
-const sum = (numbers) => {
-    const square = [];
-    let sum = 0;
-    for(const num of numbers){
-        // console.log(num)
-        const squareArray = num * num;
-        square.push(squareArray);
-        // console.log(square);
-    }
-    for( i = squareArray.length-1; i < square.length; i++){
-        let sum = sum + num;
-    }
+
+const numbers = [4, 5, 6, 8, 12]
+let sum = 0
+const getSquare = numbers.map(num => num * num);
+
+for( let num of getSquare){
+    sum = sum + num;
 }
-const numbers = sum ([4, 5, 6, 8, 12, 13, 14, 15, 16])
+const aveg = sum / getSquare.length;
+console.log(aveg)
